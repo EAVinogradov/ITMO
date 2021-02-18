a0dc3db26d0d4614c4017dfb793f9497af77ff78
EAVinogradov <e.a.vinogradov@icloud.com>
2021-02-18 16:53:10 +0300

    практическое занятие №4, упражнение №5

---
+++ ADO.NET/WorkingDataTable/WorkingDataTable/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WorkingDataTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustomersDataGridView = new System.Windows.Forms.DataGridView();
+            this.FillTableButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustomersDataGridView
+            // 
+            this.CustomersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersDataGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.CustomersDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.CustomersDataGridView.Name = "CustomersDataGridView";
+            this.CustomersDataGridView.RowHeadersWidth = 51;
+            this.CustomersDataGridView.RowTemplate.Height = 24;
+            this.CustomersDataGridView.Size = new System.Drawing.Size(800, 223);
+            this.CustomersDataGridView.TabIndex = 0;
+            // 
+            // FillTableButton
+            // 
+            this.FillTableButton.Location = new System.Drawing.Point(13, 230);
+            this.FillTableButton.Name = "FillTableButton";
+            this.FillTableButton.Size = new System.Drawing.Size(141, 47);
+            this.FillTableButton.TabIndex = 1;
+            this.FillTableButton.Text = "Fill Table";
+            this.FillTableButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FillTableButton);
+            this.Controls.Add(this.CustomersDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustomersDataGridView;
+        private System.Windows.Forms.Button FillTableButton;
+    }
+}
+
