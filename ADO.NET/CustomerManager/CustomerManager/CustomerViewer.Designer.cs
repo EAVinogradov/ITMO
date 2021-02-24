e19706f7d1bdca6c3bcb9983a93df62f8e928546
Evgeniy Vinogradov <EvgeniyVinogradov@itcenterifmo.onmicrosoft.com>
2021-02-24 21:31:19 +0300

    Customer Manager

---
+++ ADO.NET/CustomerManager/CustomerManager/CustomerViewer.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace CustomerManager
+{
+    partial class CustomerViewer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerList = new System.Windows.Forms.ComboBox();
+            this.orderlistBox = new System.Windows.Forms.ListBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelSurname = new System.Windows.Forms.Label();
+            this.labelAdress = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelPhoto = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonOut = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CustomerradioButton = new System.Windows.Forms.RadioButton();
+            this.OrderradioButton = new System.Windows.Forms.RadioButton();
+            this.labelId = new System.Windows.Forms.Label();
+            this.textBoxCustomer = new System.Windows.Forms.TextBox();
+            this.GridView = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // customerList
+            // 
+            this.customerList.FormattingEnabled = true;
+            this.customerList.Location = new System.Drawing.Point(13, 13);
+            this.customerList.Name = "customerList";
+            this.customerList.Size = new System.Drawing.Size(121, 21);
+            this.customerList.TabIndex = 0;
+            // 
+            // orderlistBox
+            // 
+            this.orderlistBox.FormattingEnabled = true;
+            this.orderlistBox.Location = new System.Drawing.Point(13, 52);
+            this.orderlistBox.Name = "orderlistBox";
+            this.orderlistBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.orderlistBox.Size = new System.Drawing.Size(120, 95);
+            this.orderlistBox.TabIndex = 1;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(151, 20);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(29, 13);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "Имя";
+            // 
+            // labelSurname
+            // 
+            this.labelSurname.AutoSize = true;
+            this.labelSurname.Location = new System.Drawing.Point(151, 52);
+            this.labelSurname.Name = "labelSurname";
+            this.labelSurname.Size = new System.Drawing.Size(56, 13);
+            this.labelSurname.TabIndex = 3;
+            this.labelSurname.Text = "Фамилия";
+            // 
+            // labelAdress
+            // 
+            this.labelAdress.AutoSize = true;
+            this.labelAdress.Location = new System.Drawing.Point(151, 84);
+            this.labelAdress.Name = "labelAdress";
+            this.labelAdress.Size = new System.Drawing.Size(38, 13);
+            this.labelAdress.TabIndex = 4;
+            this.labelAdress.Text = "Адрес";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(151, 113);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(49, 13);
+            this.labelAge.TabIndex = 5;
+            this.labelAge.Text = "Возраст";
+            // 
+            // labelPhoto
+            // 
+            this.labelPhoto.AutoSize = true;
+            this.labelPhoto.Location = new System.Drawing.Point(151, 144);
+            this.labelPhoto.Name = "labelPhoto";
+            this.labelPhoto.Size = new System.Drawing.Size(35, 13);
+            this.labelPhoto.TabIndex = 6;
+            this.labelPhoto.Text = "Фото";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(213, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(81, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(213, 81);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(81, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(213, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(81, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(213, 144);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(81, 42);
+            this.buttonFile.TabIndex = 11;
+            this.buttonFile.Text = "Выберите файл ...";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(329, 15);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 60);
+            this.buttonAdd.TabIndex = 12;
+            this.buttonAdd.Text = "Добавить данные";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonOut
+            // 
+            this.buttonOut.Location = new System.Drawing.Point(423, 15);
+            this.buttonOut.Name = "buttonOut";
+            this.buttonOut.Size = new System.Drawing.Size(75, 60);
+            this.buttonOut.TabIndex = 13;
+            this.buttonOut.Text = "Показать данные";
+            this.buttonOut.UseVisualStyleBackColor = true;
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(513, 15);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(103, 23);
+            this.buttonEdit.TabIndex = 14;
+            this.buttonEdit.Text = "Редактировать";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(513, 52);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(103, 23);
+            this.buttonDel.TabIndex = 15;
+            this.buttonDel.Text = "Удалить";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.OrderradioButton);
+            this.groupBox1.Controls.Add(this.CustomerradioButton);
+            this.groupBox1.Location = new System.Drawing.Point(329, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 51);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор данных";
+            // 
+            // CustomerradioButton
+            // 
+            this.CustomerradioButton.AutoSize = true;
+            this.CustomerradioButton.Location = new System.Drawing.Point(7, 28);
+            this.CustomerradioButton.Name = "CustomerradioButton";
+            this.CustomerradioButton.Size = new System.Drawing.Size(74, 17);
+            this.CustomerradioButton.TabIndex = 0;
+            this.CustomerradioButton.TabStop = true;
+            this.CustomerradioButton.Text = "Customers";
+            this.CustomerradioButton.UseVisualStyleBackColor = true;
+            // 
+            // OrderradioButton
+            // 
+            this.OrderradioButton.AutoSize = true;
+            this.OrderradioButton.Location = new System.Drawing.Point(155, 28);
+            this.OrderradioButton.Name = "OrderradioButton";
+            this.OrderradioButton.Size = new System.Drawing.Size(56, 17);
+            this.OrderradioButton.TabIndex = 1;
+            this.OrderradioButton.TabStop = true;
+            this.OrderradioButton.Text = "Orders";
+            this.OrderradioButton.UseVisualStyleBackColor = true;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(326, 159);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(16, 13);
+            this.labelId.TabIndex = 17;
+            this.labelId.Text = "Id";
+            // 
+            // textBoxCustomer
+            // 
+            this.textBoxCustomer.Location = new System.Drawing.Point(368, 159);
+            this.textBoxCustomer.Name = "textBoxCustomer";
+            this.textBoxCustomer.Size = new System.Drawing.Size(248, 20);
+            this.textBoxCustomer.TabIndex = 18;
+            // 
+            // GridView
+            // 
+            this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.GridView.Location = new System.Drawing.Point(0, 244);
+            this.GridView.Name = "GridView";
+            this.GridView.Size = new System.Drawing.Size(800, 206);
+            this.GridView.TabIndex = 19;
+            // 
+            // CustomerViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GridView);
+            this.Controls.Add(this.textBoxCustomer);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonOut);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonFile);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelPhoto);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelAdress);
+            this.Controls.Add(this.labelSurname);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.orderlistBox);
+            this.Controls.Add(this.customerList);
+            this.Name = "CustomerViewer";
+            this.Text = "Customer Viewer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox customerList;
+        private System.Windows.Forms.ListBox orderlistBox;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelSurname;
+        private System.Windows.Forms.Label labelAdress;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelPhoto;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonOut;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton OrderradioButton;
+        private System.Windows.Forms.RadioButton CustomerradioButton;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.TextBox textBoxCustomer;
+        private System.Windows.Forms.DataGridView GridView;
+    }
+}
+
