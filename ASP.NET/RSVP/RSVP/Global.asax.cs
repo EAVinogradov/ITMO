90d148290fb146018f3a36ba8484ef9cd7d7e973
EAVinogradov <e.a.vinogradov@icloud.com>
2021-03-02 15:41:06 +0300

    Лабораторная 1 упражнение 3

--- ASP.NET/RSVP/RSVP/Global.asax.cs
+++ ASP.NET/RSVP/RSVP/Global.asax.cs
@@ -5,12 +5,17 @@ using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
 
+
 namespace RSVP
 {
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+        }
+        protected void Application_OnEndRequest()
+        {
+            Response.Write("<hr /> Эта страница была загружена " + DateTime.Now.ToString());
         }
     }
 }
