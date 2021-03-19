513a2ee11e01905beb0eb329567a850836645b3a
Evgeniy Vinogradov <EvgeniyVinogradov@itcenterifmo.onmicrosoft.com>
2021-03-19 19:12:15 +0300

    Lab1, Упражнение 3

--- ASP.NET Core Razor Page/WebAppCoreProduct/WebAppCoreProduct/Pages/Index.cshtml.cs
+++ ASP.NET Core Razor Page/WebAppCoreProduct/WebAppCoreProduct/Pages/Index.cshtml.cs
@@ -14,14 +14,37 @@ namespace WebAppCoreProduct.Pages
         //public decimal? Price { get; set; }
         public bool IsCorrect { get; set; } = true;
         public Product Product { get; set; }
-        public void OnGet(string name, decimal? price)
+        public string MessageRezult { get; private set; }
+
+        //public void OnGet(string name, decimal? price)
+        //{
+        //    Product = new Product();
+
+        //    if (price == null || price < 0 || string.IsNullOrEmpty(name))
+        //    {
+        //        IsCorrect = false;
+        //        return;
+        //    }
+        //    var result = price * (decimal?)0.18;
+        //    MessageRezult = $"Для товара {name} с ценой {price} скидка получится { result}";
+        //    Product.Price = price;
+        //    Product.Name = name;
+        //}
+        public void OnGet()
+        {
+            MessageRezult = "Для товара можно определить скидку";
+        }
+
+        public void OnPost(string name, decimal? price)
         {
             Product = new Product();
             if (price == null || price < 0 || string.IsNullOrEmpty(name))
             {
-                IsCorrect = false;
-                return;
+                MessageRezult = "Переданы некорректные данные. Повторите ввод";
+            return;
             }
+            var result = price * (decimal?)0.18;
+            MessageRezult = $"Для товара {name} с ценой {price} скидка получится { result}";
             Product.Price = price;
             Product.Name = name;
         }
