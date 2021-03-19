e0d5a0c031bd4daf80874baaaa57a205d81d07ec
Evgeniy Vinogradov <EvgeniyVinogradov@itcenterifmo.onmicrosoft.com>
2021-03-19 19:27:41 +0300

    Lab1, Упражнение 4

--- ASP.NET Core Razor Page/WebAppCoreProduct/WebAppCoreProduct/Pages/Product.cshtml.cs
+++ ASP.NET Core Razor Page/WebAppCoreProduct/WebAppCoreProduct/Pages/Product.cshtml.cs
@@ -8,31 +8,13 @@ using WebAppCoreProduct.Models;
 
 namespace WebAppCoreProduct.Pages
 {
-    public class IndexModel : PageModel
+    public class ProductModel : PageModel
     {
-        //public string Name { get; set; }
-        //public decimal? Price { get; set; }
-        public bool IsCorrect { get; set; } = true;
         public Product Product { get; set; }
         public string MessageRezult { get; private set; }
-
-        //public void OnGet(string name, decimal? price)
-        //{
-        //    Product = new Product();
-
-        //    if (price == null || price < 0 || string.IsNullOrEmpty(name))
-        //    {
-        //        IsCorrect = false;
-        //        return;
-        //    }
-        //    var result = price * (decimal?)0.18;
-        //    MessageRezult = $"Для товара {name} с ценой {price} скидка получится { result}";
-        //    Product.Price = price;
-        //    Product.Name = name;
-        //}
         public void OnGet()
         {
-            //MessageRezult = "Для товара можно определить скидку";
+            MessageRezult = "Для товара можно определить скидку";
         }
 
         public void OnPost(string name, decimal? price)
@@ -41,12 +23,12 @@ namespace WebAppCoreProduct.Pages
             if (price == null || price < 0 || string.IsNullOrEmpty(name))
             {
                 MessageRezult = "Переданы некорректные данные. Повторите ввод";
-            return;
+                return;
             }
             var result = price * (decimal?)0.18;
             MessageRezult = $"Для товара {name} с ценой {price} скидка получится { result}";
             Product.Price = price;
             Product.Name = name;
         }
-    }
+}
 }
