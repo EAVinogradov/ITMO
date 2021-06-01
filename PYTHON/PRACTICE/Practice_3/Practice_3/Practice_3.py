056e76653035a72b4e31b5169825a006eea61cf0
EAVinogradov <e.a.vinogradov@icloud.com>
2021-06-01 12:53:11 +0300

    Add pracrice_3

---
+++ PYTHON/PRACTICE/Practice_3/Practice_3/Practice_3.py
@@ -0,0 +1,20 @@
+import statistics
+import sys
+import math
+import random
+print(dir(math))
+print(dir(statistics))
+list_1=[random.randint(0,10) for i in range (10)]
+print(list_1)
+print(math.fsum(list_1))
+print(statistics.fmean(list_1))
+print(statistics.median(list_1))
+print(statistics.stdev(list_1))
+print('--------------\n')
+print('Задание 4')
+num=random.randint(1,100)
+print(num)
+
+
+
+
